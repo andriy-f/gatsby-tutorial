397ba1e00dc96a272b4ab0052e6712a844fb6869
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-13 20:00:35 +0000

    add about page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>About Me</h1>
+      <p>I’m good, thank you</p>
+    </main>
+  )
+}
+
+export default AboutPage

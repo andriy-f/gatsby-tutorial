4b7653c0c21c18dbe68e0802c0ca1aec9e9158d0
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-14 15:26:23 +0000

    add external image via StaticImage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Layout from '../components/layout'
+import { StaticImage } from 'gatsby-plugin-image'
 
 const pageTitle = "Home Page"
 const IndexPage = () => {
   return (
     <Layout pageTitle={pageTitle}>
       <p>I'm making this by following the Gatsby Tutorial.</p>
+      <StaticImage
+        alt="Yoga class"
+        src="https://yogalifestyle.info/wp-content/uploads/Yoga-class.jpeg"
+      />
     </Layout>
   )
 }

9b7c079912b11bd1d163bdf25ac600531f4b778f
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-14 14:41:33 +0000

    add styles to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import { Link } from 'gatsby'
-import { container as containerCSS } from './layout.module.css'
+import {
+  container as containerCSS,
+  heading, navLinks, navLinkItem,
+  navLinkText
+} from './layout.module.css'
 
 const Layout = ({ pageTitle, children }) => {
   return (
     <div className={containerCSS}>
       <header>
         <nav>
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/about">About</Link></li>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/">Home</Link></li>
+            <li className={navLinkItem}>
+              <Link className={navLinkText} to="/about">About</Link></li>
           </ul>
         </nav>
       </header>
       <main>
-        <h1>{pageTitle}</h1>
+        <h1 className={heading}>{pageTitle}</h1>
         {children}
       </main>
       <footer>

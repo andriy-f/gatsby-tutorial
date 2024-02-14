65ff359ba32716115cbad82c0db71d045dec8c76
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-14 13:56:27 +0000

    add links

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,9 +1,11 @@
 import * as React from 'react'
+import { Link } from 'gatsby'
 
 const AboutPage = () => {
   return (
     <main>
       <h1>About Me</h1>
+      <Link to="/">Back to Home</Link>
       <p>I’m good, thank you</p>
     </main>
   )

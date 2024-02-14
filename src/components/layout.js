229d83c5017d485c200701cca4728bfd3fed3b05
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-14 14:15:28 +0000

    add layout component

---
+++ src/components/layout.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Layout = ({ pageTitle, children }) => {
+  return (
+    <div className="layout">
+      <header>
+        <nav>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>{pageTitle}</h1>
+        {children}
+      </main>
+      <footer>
+        {/* Footer content */}
+      </footer>
+    </div>
+  )
+}
+
+export default Layout

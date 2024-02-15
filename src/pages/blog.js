de30fc275b9f9891a67f8403648c51ea2322a19e
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-02-15 19:57:11 +0000

    show blog title

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -3,9 +3,11 @@ import React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
 
+const pageTitle = 'My Blog Posts'
+
 const BlogPage = () => {
   return (
-    <Layout>
+    <Layout pageTitle={pageTitle}>
       <p>Blog Posts will show up here later on.</p>
     </Layout>
   )

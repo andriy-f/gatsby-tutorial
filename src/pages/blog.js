eaf4685f23c3ef6f7bfba73ce1350341b58d8ffc
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-03-04 14:36:17 +0000

    updated blog style

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -9,13 +9,12 @@ const pageTitle = 'My Blog Posts'
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle={pageTitle}>
-      <ul>
-        {data.allMdx.nodes.map(node => (
-          <li key={node.id}>
-            {node.frontmatter.title} ({node.frontmatter.date})
-          </li>)
-        )}
-      </ul>
+      {data.allMdx.nodes.map(node => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+        </article>)
+      )}
     </Layout>
   )
 }

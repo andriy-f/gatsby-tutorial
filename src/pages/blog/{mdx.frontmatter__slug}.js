b7b8afc23d93aa854476c768fff0b613353b7419
Andriy Fetsyuk <1040440+andriy-f@users.noreply.github.com>
2024-03-05 16:22:57 +0000

    display image on individual blog posts

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,17 +1,22 @@
 import * as React from 'react'
-// import { graphql } from 'gatsby'
+import { graphql } from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
+
 
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost = (props) => {
+  const image = getImage(props.data.mdx.frontmatter.hero_image)
+  const frontmatter = props.pageContext.frontmatter
   return (
     // <Layout pageTitle={props.data.mdx.frontmatter.title}>
     //   Posted: {props.data.mdx.frontmatter.date}
     //   {props.children}
     // </Layout>
-    <Layout pageTitle={props.pageContext.frontmatter.title}>
-      Posted: {props.pageContext.frontmatter.date}
+    <Layout pageTitle={frontmatter.title}>
+      Posted: {frontmatter.date}
+      <GatsbyImage image={image} alt={frontmatter.hero_image_alt} />
       {props.children}
     </Layout>
   )
@@ -23,14 +28,20 @@ export const Head = (props) => {
   )
 }
 
-// export const query = graphql`
-// query ($id: String) {
-//   mdx(id: {eq: $id}) {
-//     frontmatter {
-//       title
-//       date(formatString: "D MMM YYYY")
-//     }
-//   }
-// }
-// `
+export const query = graphql`
+query ($id: String) {
+  mdx(id: {eq: $id}) {
+    frontmatter {
+      title
+      date(formatString: "D MMM YYYY")
+      hero_image_alt
+      hero_image {
+        childImageSharp {
+          gatsbyImageData
+        }
+      }
+    }
+  }
+}
+`
 export default BlogPost
